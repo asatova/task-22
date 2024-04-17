3a5cfe19c43d456ed61676eb100f83d4364fabeb
zarnigor <zarnigorasatova52@gmail.com>
2024-04-17 17:48:58 +0500

    feature

--- app.js
+++ app.js
@@ -1,30 +1,24 @@
 "use strict"
 // 1- task
-function right2(str) {
-    if (str.length <= 2) {
-        return str;
-    } else {
-        return str.slice(-2) + str.slice(0, -2);
-    }
-}
-
-console.log(right2('Hello'));
-console.log(right2('java'));
-console.log(right2('Hi'));
+// function right2(str) {
+//     if (str.length <= 2) {
+//         return str;
+//     } else {
+//         return str.slice(-2) + str.slice(0, -2);
+//     }
+// }
+//
+// console.log(right2('Hello'));
+// console.log(right2('java'));
+// console.log(right2('Hi'));
 
 // 2-task
 
-const englishWords = ["hi","bye"]
-englishWords.push("bye","hi")
-console.log(englishWords);
+function makeAbba(a, b) {
+    return a + b + b + a;
+}
 
-
-
-
-const englishNames = ["Yo", "Alice"]
-englishNames.push("Alice","Yo")
-console.log(englishNames);
-
-const englishidioms = ["what", "Up"]
-englishidioms.push("Up","What")
-console.log(englishidioms);
+// Test qismi
+console.log(makeAbba('Hi', 'Bye')); // HiByeByeHi
+console.log(makeAbba('Yo', 'Alice')); // YoAliceAliceYo
+console.log(makeAbba('What', 'Up')); // WhatUpUpWhat
